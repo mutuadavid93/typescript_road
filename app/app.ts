e2265bd0a29faa44d82306134a9811787a86d37c
David Mutua <mutuadavi@gmail.com>
2018-05-19 10:11:44 +0200

    structured out the lirary management app

--- app/app.ts
+++ app/app.ts
@@ -1,10 +1,33 @@
-class HelloTypescript {
-    constructor(public message: string){}
 
-    greating(): string {
-        return this.message;
-    }
+function GetAllBooks(): any[] {
+
+    // Book List.
+    let books = [
+        {title: 'Tear Drop', author: 'Giannis Antetokunmpo', available: true},
+        {title: 'Long Travel', author: 'Lillard Damian', available: false},
+        {title: 'Heavy Dunk', author: 'James Harden', available: true},
+        {title: 'Deep Bucket', author: 'Clay Thompson', available: true},
+    ];
+
+    return books;
 }
 
-let hellotsc = new HelloTypescript("Hello Typescript?");
-console.log(hellotsc.greating());
+function logFirstAvailable (books: any[]): void {
+
+    // Store the number of Books in the Array
+    let numberOfBooks:number = books.length;
+    let firstAvailableBkTitle:string = '';
+
+    for (let curBook of books) {
+        if(curBook.available) {
+            firstAvailableBkTitle = curBook.title;
+            break;
+        }
+    }
+
+    console.log(`Total Books: ${numberOfBooks}`);
+    console.log(`First Available Book: ${firstAvailableBkTitle}`);
+}
+
+const allBooks = GetAllBooks();
+logFirstAvailable(allBooks);
--- js/app.js
+++ js/app.js
@@ -1,12 +1,27 @@
-var HelloTypescript = /** @class */ (function () {
-    function HelloTypescript(message) {
-        this.message = message;
+function GetAllBooks() {
+    // Book List.
+    var books = [
+        { title: 'Tear Drop', author: 'Giannis Antetokunmpo', available: true },
+        { title: 'Long Travel', author: 'Lillard Damian', available: false },
+        { title: 'Heavy Dunk', author: 'James Harden', available: true },
+        { title: 'Deep Bucket', author: 'Clay Thompson', available: true },
+    ];
+    return books;
+}
+function logFirstAvailable(books) {
+    // Store the number of Books in the Array
+    var numberOfBooks = books.length;
+    var firstAvailableBkTitle = '';
+    for (var _i = 0, books_1 = books; _i < books_1.length; _i++) {
+        var curBook = books_1[_i];
+        if (curBook.available) {
+            firstAvailableBkTitle = curBook.title;
+            break;
+        }
     }
-    HelloTypescript.prototype.greating = function () {
-        return this.message;
-    };
-    return HelloTypescript;
-}());
-var hellotsc = new HelloTypescript("Hello Typescript?");
-console.log(hellotsc.greating());
+    console.log("Total Books: " + numberOfBooks);
+    console.log("First Available Book: " + firstAvailableBkTitle);
+}
+var allBooks = GetAllBooks();
+logFirstAvailable(allBooks);
 //# sourceMappingURL=app.js.map
